34e6ec47aa38239ceb94c3d190044bbd72951771
rebe-ov <yrov92@gmail.com>
2025-03-09 21:05:34 -0600

    Creación del proyecto y solution para el laboratorio 1

---
+++ Laboratorio1_suCarne/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_suCarne.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
